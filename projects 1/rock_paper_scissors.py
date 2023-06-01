f1a4fedbe621f9667f977ac2d24964cf98707e5c
Ckaeiront <stampedesw29@gmail.com>
2023-06-01 03:42:18 -1000

    rock papers v1.1.0

--- projects 1/rock_paper_scissors.py
+++ projects 1/rock_paper_scissors.py
@@ -1,24 +1,27 @@
 import random
 choices = ['rock', 'paper', 'scissors']
-c = 1
-try:
-    while c == 1:
-        userChoices = int(
-            input('choose from 1 to 3:\n1 - Rock 2 - Paper 3 - Scissors '))
-        if userChoices > 3:
-            print('number above the specified')
-        userChoices = choices[userChoices - 1]
-        computerChoice = random.choice(choices)
-        results = [userChoices, computerChoice]
-        if results[0] == 'rock' and results[1] == 'scissors':
-            print('you choose ' + results[0] + ' against ' + results[1] + '...\nYou won! 😆')
-        elif results[0] == 'paper' and results[1] == 'rock':
-            print('you choose ' + results[0] + ' against ' + results[1] + '...\nYou won! 😆')
-        elif results[0] == 'scissors' and results[1] == 'paper':
-            print('you choose ' + results[0] + ' against ' + results[1] + '...\nYou won! 😆')
-        elif results[0] == results[1]:
-            print('you choose ' + results[0] + ' against ' + results[1] + '...\nDraw!')
-        else:
-            print('you choose ' + results[0] + ' against ' + results[1] + '...\nYou lost! 😔')
-except ValueError:
-    print('Please, type a number')
+while True:
+    userChoices = int(
+        input('choose from 1 to 3:\n1 - Rock 2 - Paper 3 - Scissors '))
+    if userChoices > 3 or userChoices < 0:
+        print('invalid number')
+    userChoices = choices[userChoices - 1]
+    computerChoice = random.choice(choices)
+    results = [userChoices, computerChoice]
+    if results[0] == 'rock' and results[1] == 'scissors':
+        print('you choose ' + results[0] + ' against ' + results[1] + '...\nYou won! 😆')
+    elif results[0] == 'paper' and results[1] == 'rock':
+        print('you choose ' + results[0] + ' against ' + results[1] + '...\nYou won! 😆')
+    elif results[0] == 'scissors' and results[1] == 'paper':
+        print('you choose ' + results[0] + ' against ' + results[1] + '...\nYou won! 😆')
+    elif results[0] == results[1]:
+        print('you choose ' + results[0] + ' against ' + results[1] + '...\nDraw!')
+    else:
+        print('you choose ' + results[0] + ' against ' + results[1] + '...\nYou lost! 😔')
+    playAgain = int(input('you wanna play again?:\n1 - Yes / 2 - No '))
+    if playAgain != 1:
+        break
+
+# 👆 my version of the code
+
+
