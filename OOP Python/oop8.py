16f34b7ac4b667bbbeec756b0e77e225ffeba790
Ckaeiront <stampedesw29@gmail.com>
2023-06-04 18:31:29 -1000

    OOP v1.5.0

--- OOP Python/oop8.py
+++ OOP Python/oop8.py
@@ -12,14 +12,21 @@ class Vehicle(ABC):
     @abstractmethod
     def go(self):
         pass
+    @abstractmethod
+    def stop(self):
+        pass
 
 class Car(Vehicle):
     def go(self):
         print('you drive the car')
+    def stop(self):
+        print('this car stopped')
 
 class Motorcycle(Vehicle):
     def go(self):
         print('you ride the motorcycle')
+    def stop(self):
+        print('this motorcycle stopped')
 
 # vehicle = Vehicle()
 car = Car()
