f28391e23c942e988496c90aebdc7bdc5c6841a1
Ckaeiront <stampedesw29@gmail.com>
2023-06-12 02:47:17 -0300

    OOP v2.0

---
+++ 4 - h/walrus.py
@@ -0,0 +1,12 @@
+#   walrus operator :=
+
+#   new to python 3.8
+#   assignment expression aka walrus operator
+#   assigns values to variables as part of a larger expression
+
+#   foods = list()
+
+# happy = True
+# print(happy)
+
+print(happy := True)
